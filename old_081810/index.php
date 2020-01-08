150a176015790ebb7a5776c7710f5c1bbddf91c7
Iusr_3707326 <Iusr_3707326@p3nlhftpg356.shr.prod.phx3.secureserver.net>
2020-01-08 11:19:42 -0700

    first commit

---
+++ old_081810/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Eliana de Leon Interior Design</title>
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<link rel="stylesheet" type="text/css" href="css/lightbox.css">
+<script type="text/javascript" src="js/lightbox.js"></script>
+</head>
+
+<body>
+
+<div id="container">
+
+	<h1><img src="images/title_left4.jpg" alt="Eliana de Leon Interior Design" /></h1>
+ 
+ 	<div class="contact">
+    	<p>Los Angeles, CA: 310 503 4465</p>
+        <p>Paso Robles, CA: 805 237 0011</p>
+        <p>Email: <a href="mailto:elid45@aol.com" title="Contact Me">elid45@aol.com</a></p>
+    </div>
+    
+    <div id="pictures">
+   	  <a href="images/photo5_big.jpg" rel="lightbox" title=""><img src="images/photo5.jpg" /></a>
+      <a href="images/photo2_big.jpg" rel="lightbox" title=""><img src="images/photo2.jpg" /></a>
+      <a href="images/photo3_big.jpg" rel="lightbox" title=""><img src="images/photo3.jpg" /></a>
+      <a href="images/photo1_big.jpg" rel="lightbox" title=""><img src="images/photo1.jpg" /></a>
+    </div>
+    
+    <div class="clear"></div>
+    
+    <h2>Eliana de Leon, Interior Designer</h2>
+    
+    <div class="contact" id="b-contact">
+        <p>Los Angeles, CA:  310 503 4465</p>
+        <p>Paso Robles, CA:  805 237 0011</p>
+        <p>Email: <a href="mailto:elid45@aol.com" title="Contact Me">elid45@aol.com</a></p>
+        <p><img src="images/portrait.jpg" /></p>
+      <p>website: <a href="http://www.elianainteriordesign.com">www.elianainteriordesign.com</a></p>
+    </div>
+    
+    <div id="bio">
+    	
+         <p>The desire to create comfortable and visually pleasing living spaces has existed for centuries. Creativity is the most important skill an interior designer can possess.  A designer also needs to be responsive to client wants, have communication skills and create in diverse styles.</p>
+      	<p>With 17 years of experience I bring a strong sense of color, texture, pattern and proportion to my projects. I embrace the idea of eclecticism to create dynamic environments. A classical vase can
+      	be placed with Baroque art and antique Moroccan pillows can sit on a modern sofa.</p>
+		<p>My projects cover a spectrum of budgets. Everything does not need to be done at the same level
+        of richness. Mixing old and new, high cost and low cost creates contrast and therefore creates interest.</p>
+		<p>My interior design services range from limited changes to complete renovations. My fee is based on the complexity of the project. Additionally, I am available as a staging consultant on an hourly basis. I can access the current design and recommend the improvements via staging or redesigning. He vivido y trabajado como decoradora en america latina, y hablo espa&ntilde;ol.</p>
+    </div>
+    
+    
+    
+</div>
+
+</body>
+</html>
---
+++ old_081810/index_old.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Eliana de Leon Interior Design</title>
+<link rel="stylesheet" type="text/css" href="css/styles_old.css">
+<link rel="stylesheet" type="text/css" href="css/lightbox.css">
+<script type="text/javascript" src="js/lightbox.js"></script>
+</head>
+
+<body onload="initLightbox()">
+
+<div id="container">
+
+	<h1><img src="images/title.jpg" alt="Eliana de Leon Interior Design" /></h1>
+ 
+ 	<div class="contact">
+    	<p>Los Angeles, CA</p>
+        <p>v. 310 546 7596</p>
+        <p>f. 310 545 7130</p>
+        <p>e. <a href="mailto:elid45@aol.com" title="Contact Me">elid45@aol.com</a></p>
+    </div>
+    
+    <div id="pictures">
+    	<a href="images/demo1_big.jpg" rel="lightbox" title=""><img src="images/demo1.jpg" /></a>
+        <a href="images/demo2_big.jpg" rel="lightbox" title=""><img src="images/demo2.jpg" /></a>
+        <a href="images/demo3_big.jpg" rel="lightbox" title=""><img src="images/demo3.jpg" /></a>
+        <a href="images/demo4_big.jpg" rel="lightbox" title=""><img src="images/demo4.jpg" /></a>
+    </div>
+    
+    <div class="clear"></div>
+    
+    <h2>Eliana de Leon Interior Design</h2>
+    
+    <div class="contact" id="b-contact">
+    	<p>Los Angeles, CA</p>
+        <p>v. 310 546 7596</p>
+        <p>f. 310 545 7130</p>
+        <p>e. <a href="mailto:elid45@aol.com" title="Contact Me">elid45@aol.com</a></p>
+        <p>web. <a href="http://www.elianainteriordesign.com">www.elianainteriordesign.com</a></p>
+        <p><img src="images/portrait.jpg" /></p>
+    </div>
+    
+    <div id="bio">
+    	<p>The desire to create comfortable and visually pleasing living spaces has existed for centuries. Creativity is the most important skill an interior designer can possess. A designer requires organizational skills, the ability to prioritize, problem solving skills, the ability to work within a budget, self discipline and attention to detail. With 15 years of experience I bring a strong sense of color, balance, texture, pattern and proportion to my projects. I am familiar with modern and historical design styles. I embrace the idea of eclecticism - mixing various styles to create an aesthetic diverse environment. A classical vase can be placed with Baroque art and antique moroccan pillows can sit on a modern sofa.</p>
+        <p>My projects cover a spectrum of budgets. I understand that everything does not need to be done at the same level of richness. Mixing old and new, high cost and low cost creates contrast and creates interest. It is not necessary to fill a room with furniture and accessories - I strive for quality not quantity.</p>
+        <p>As an independent contractor my design services range from limited changes to complete renovations. I am also available as a consultant on an hourly basis. <br />
+My fee is based on the complexity of the project.</p>
+		<p>Tambien he trabajado en America Latina, y hablo espanol.</p>
+    </div>
+    
+    
+    
+</div>
+
+</body>
+</html>
